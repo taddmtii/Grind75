2a4338cd3a543f9c4909aaf30af1bbecb7c657b4
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-01 12:59:54 -0800

    added the more files

---
+++ Week1/merge-two-sorted-lists.py
@@ -0,0 +1,19 @@
+def mergeTwoLists(list1, list2):
+    dummy = ListNode()
+    curr = dummy
+
+    while list1 and list2:
+        if list1.val > list2.val:
+            curr.next = list2
+            list2 = list2.next
+        else:
+            curr.next = list1
+            list1 = list1.next
+        curr = curr.next
+
+    if list1:
+        curr.next = list1
+    else:
+        curr.next = list2
+
+    return dummy.next
