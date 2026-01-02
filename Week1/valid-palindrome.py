9b04ea55ba45764346b2f309c155f87a08ed39bb
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-02 08:05:18 -0800

    added more files

--- Week1/valid-palindrome.py
+++ Week1/valid-palindrome.py
@@ -1,2 +1,15 @@
-def isPalindrome(s):
-    
+def isPalindrome(self, s: str) -> bool:
+    cleaned = ""
+    for ch in s:
+        if ch.isalnum():
+            cleaned += ch.lower()
+    l = 0
+    r = len(cleaned) - 1
+
+    for _ in range(len(cleaned) - 1):
+        if cleaned[l] == cleaned[r]:
+            l += 1
+            r -= 1
+        else:
+            return False
+    return True   
