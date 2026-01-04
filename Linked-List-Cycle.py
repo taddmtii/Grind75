2d752409bcf25a5d558ded5fcfe1ecf7a22d1ee6
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-01-04 10:57:38 -0800

    [141] [Time Beats: 17.40%] [Memory Beats: 95.08%] - LeetPush

---
+++ Linked-List-Cycle.py
@@ -0,0 +1,19 @@
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.next = None
+6
+7class Solution:
+8    def hasCycle(self, head: Optional[ListNode]) -> bool:
+9        if not head:
+10            return
+11        slow = head
+12        fast = head.next
+13
+14        while slow and fast and fast.next:
+15            if slow == fast:
+16                return True
+17            slow = slow.next
+18            fast = fast.next.next
+19        return False
