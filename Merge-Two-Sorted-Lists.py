7247bf724b283853c98840f78972787af668d308
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-01-04 10:54:28 -0800

    [21] [Time Beats: 18.35%] [Memory Beats: 79.38%] - LeetPush

---
+++ Merge-Two-Sorted-Lists.py
@@ -0,0 +1,25 @@
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+8        dummy = ListNode()
+9        curr = dummy
+10
+11        while list1 and list2:
+12            if list1.val > list2.val:
+13                curr.next = list2
+14                list2 = list2.next
+15            else:
+16                curr.next = list1
+17                list1 = list1.next
+18            curr = curr.next
+19
+20        if list1:
+21            curr.next = list1
+22        else:
+23            curr.next = list2
+24
+25        return dummy.next
