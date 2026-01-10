9112acb48bbf49107bde3cb2a26a7310e84dfc8b
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-01-10 12:42:50 -0500

    [21] [Time Beats: 100.00%] [Memory Beats: 5.00%] - LeetPush

--- Merge-Two-Sorted-Lists.py
+++ Merge-Two-Sorted-Lists.py
@@ -6,20 +6,20 @@
 6class Solution:
 7    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 8        dummy = ListNode()
-9        curr = dummy
+9        merged = dummy
 10
 11        while list1 and list2:
 12            if list1.val > list2.val:
-13                curr.next = list2
+13                merged.next = list2
 14                list2 = list2.next
-15            else:
-16                curr.next = list1
+15            elif list1.val <= list2.val:
+16                merged.next = list1
 17                list1 = list1.next
-18            curr = curr.next
+18            merged = merged.next
 19
 20        if list1:
-21            curr.next = list1
-22        else:
-23            curr.next = list2
+21            merged.next = list1
+22        elif list2:
+23            merged.next = list2
 24
 25        return dummy.next
