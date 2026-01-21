a57b0978a0dde08c66d48b3fee11a4873a34cbb2
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-01-21 09:08:45 -0500

    [733] [Time Beats: 100.00%] [Memory Beats: 7.41%] - LeetPush

--- Flood-Fill.py
+++ Flood-Fill.py
@@ -1,19 +1,22 @@
 1class Solution:
 2    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 3        seen = set()
-4        original_color = image[sr][sc]
-5        num_rows, num_cols = len(image), len(image[0])
+4        startingColor = image[sr][sc]
+5        
 6
 7        def dfs(row, col):
-8            if (row, col) in seen or row < 0 or row >= num_rows or col < 0 or col >= num_cols:
-9                return
-10            seen.add((row, col))
-11            if image[row][col] == original_color:
-12                image[row][col] = color
-13                dfs(row + 1, col)
-14                dfs(row - 1, col)
-15                dfs(row, col + 1)
-16                dfs(row, col - 1)
-17
-18        dfs(sr, sc)
-19        return image
+8            if row >= len(image) or row < 0 or col >= len(image[0]) or col < 0 or (row, col) in seen or image[row][col] != startingColor:
+9                return 
+10            
+11            image[row][col] = color
+12            seen.add((row, col))
+13            
+14            dfs(row + 1, col)
+15            dfs(row - 1, col)
+16            dfs(row, col + 1)
+17            dfs(row, col - 1) 
+18        
+19
+20        dfs(sr, sc)
+21        return image
+22
