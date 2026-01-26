71e0d919d8cae3017a8b9c798e3fbd292868ba94
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-01-26 14:39:16 -0500

    [169] [Time Beats: 88.28%] [Memory Beats: 18.96%] - LeetPush

--- Majority-Element.py
+++ Majority-Element.py
@@ -1,9 +1,9 @@
 1class Solution:
 2    def majorityElement(self, nums: List[int]) -> int:
 3        nums_map = Counter(nums)
-4        n = len(nums) / 2
-5        for num in nums:
-6            if nums_map[num] > n:
-7                return num
-8
-9
+4        majority = len(nums) / 2
+5        
+6        for elem in nums_map:
+7            if nums_map[elem] >= majority:
+8                return elem
+9        
