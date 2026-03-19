65e317e69d2ba8eb3c2e80c5db9f17c02fdc4256
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-03-18 21:11:57 -0400

    [242] [Time Beats: 90.62%] [Memory Beats: 89.44%] - LeetPush

--- Valid-Anagram.py
+++ Valid-Anagram.py
@@ -1,11 +1,6 @@
 1class Solution:
 2    def isAnagram(self, s: str, t: str) -> bool:
-3        if len(s) != len(t):
-4            return False
-5        s_map = Counter(s)
-6        t_map = Counter(t)
-7
-8        for s in s_map:
-9            if s not in t_map or t_map[s] != s_map[s]:
-10                return False
-11        return True
+3        s_map = Counter(s)
+4        t_map = Counter(t)
+5
+6        return s_map == t_map
