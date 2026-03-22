6ef650c879bdd0fc7f8254773825c5db0a5a83f7
Tadd Trumbull II <122894305+taddmtii@users.noreply.github.com>
2026-03-21 21:02:36 -0400

    [125] [Time Beats: 50.30%] [Memory Beats: 97.71%] - LeetPush

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -7,9 +7,10 @@
 7        
 8        l, r = 0, len(cleaned) - 1
 9
-10        while l <= r:
-11            if cleaned[l] != cleaned[r]:
-12                return False
-13            l += 1
-14            r -= 1
-15        return True
+10        for _ in range(len(cleaned)):
+11            if cleaned[l] == cleaned[r]:
+12                l += 1
+13                r -= 1
+14            else:
+15                return False
+16        return True
